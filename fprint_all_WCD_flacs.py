2149fd96d5ab2f40391778457e5d76f668b8df67
Dan Ellis <dpwe@ee.columbia.edu>
2014-07-21 15:37:15 +0000

    first complete version to read flaclist then attempt to run fingerprint over all of it

--- fprint_all_WCD_flacs.py
+++ fprint_all_WCD_flacs.py
@@ -8,7 +8,7 @@
 import os
 import sys
 import pickle
-#import get_MSD_audio
+import get_MSD_audio
 import numpy as np
 
 sys.path.append('../python/audfprint')
@@ -25,30 +25,49 @@ import gzip
 # list of all the archive items that exist
 flaclist = 'flaclist2.txt'
 
-# Read in the list of all flac items
-names = []
-nfails = 0
-with codecs.getreader('utf-8')(gzip.open(flaclist+'.gz', 'rb')) as fp:
-    for item in fp:
-        item = item.rstrip('\n')
-        if item[:7] == "#artist":
-            # header
-            pass
-        else:
-            try:
-                artist, album, title, length, iaitem, name = item.split('\t')
-                names.append(name)
-            except:
-                #print "%d: problem splitting %s" % (len(names), item)
-                nfails += 1
+def read_all_flacs(flaclist):
+    """ Read in the list of all flac items """
+    names = []
+    nfails = 0
+    with codecs.getreader('utf-8')(gzip.open(flaclist+'.gz', 'rb')) as fp:
+        for item in fp:
+            item = item.rstrip('\n')
+            if item[:7] == "#artist":
+                # header
+                pass
+            else:
+                try:
+                    artist, album, title, length, iaitem, name = item.split('\t')
+                    names.append(os.path.join(iaitem, name))
+                except:
+                    #print "%d: problem splitting %s" % (len(names), item)
+                    nfails += 1
 
-print "%d items read, %d skipped" % (len(names), nfails)
+    print "%d items read, %d skipped" % (len(names), nfails)
+    return names
 
-def make_fprint(WCD_archive, WCD_path):
+def ensure_dir(fname):
+    """ ensure that the directory for the named path exists """
+    head, tail = os.path.split(fname)
+    if len(head):
+        if not os.path.exists(head):
+            os.makedirs(head)
+
+def make_fprint(WCD_name):
     """ calculate and store the fprint for one item """
-    fname = get_MSD_audio.IA_audio_file(WCD_archive, WCD_path)
-    hashes = audfprint.wavfile2hashes(fname)
-    opfname = os.path.join(fp_dir, fp_path)
-    audfprint.hashes_save(opfname, hasehs)
+    audio_file = get_MSD_audio.IA_audio_file(WCD_name)
+    hashes = audfprint.wavfile2hashes(audio_file)
+    WCD_root, WCD_ext = os.path.splitext(WCD_name)
+    opfname = os.path.join(fp_dir, WCD_root+'.afp')
+    ensure_dir(opfname)
+    audfprint.hashes_save(opfname, hashes)
     print "wrote",opfname
     # Delete the files downloaded?
+    get_MSD_audio.remove_audio(audio_file)
+
+
+# Run the whole thing
+flacs = read_all_flacs(flaclist)
+
+joblib.Parallel(n_jobs=4)(joblib.delayed(make_fprint)(WCD_name)
+                          for WCD_name in flacs)
